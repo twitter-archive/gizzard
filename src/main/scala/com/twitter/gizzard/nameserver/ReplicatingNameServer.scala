c2ea4201269120ff999de1331a5c0f07975e49f5
Ed Ceaser <eac@twitter.com>
2010-03-24 15:57:00 -0700

    Add default shardinfo and weight to ReplicatingNameServer

--- src/main/scala/com/twitter/gizzard/nameserver/ReplicatingNameServer.scala
+++ src/main/scala/com/twitter/gizzard/nameserver/ReplicatingNameServer.scala
@@ -4,6 +4,8 @@ import shards._
 import net.lag.logging.ThrottledLogger
 
 
-class ReplicatingNameServer(shardInfo: ShardInfo, weight: Int, replicas: Seq[ManagingNameServer], log: ThrottledLogger[String], future: Future)
-  extends ReplicatingShard(shardInfo, weight, replicas, new LoadBalancer[ManagingNameServer](replicas), log, future, None)
+class ReplicatingNameServer(replicas: Seq[ManagingNameServer], log: ThrottledLogger[String], future: Future)
+  extends ReplicatingShard(
+    new ShardInfo("com.twitter.gizzard.nameserver.ReplicatingNameServer", "", ""),
+    1, replicas, new LoadBalancer[ManagingNameServer](replicas), log, future, None)
   with ReadWriteNameServer
