1099a0e467fb81e62c11b04a2942d6b630769062
Robey Pointer <robey@twitter.com>
2010-03-30 17:39:12 -0700

    just pass in a method to collect events. we don't care about that bogus event system anymore.

--- src/main/scala/com/twitter/gizzard/shards/ReplicatingShard.scala
+++ src/main/scala/com/twitter/gizzard/shards/ReplicatingShard.scala
@@ -7,12 +7,11 @@ import scala.collection.mutable
 import scala.util.Sorting
 import net.lag.logging.{Logger, ThrottledLogger}
 import com.twitter.gizzard.thrift.conversions.Sequences._
-import com.twitter.ostrich.W3CReporter
 
 
 class ReplicatingShard[ConcreteShard <: Shard](val shardInfo: ShardInfo, val weight: Int,
   val children: Seq[ConcreteShard], loadBalancer: (() => Seq[ConcreteShard]),
-  log: ThrottledLogger[String], future: Future, eventLogger: Option[W3CReporter])
+  log: ThrottledLogger[String], future: Future, eventLogger: (String, String) => Unit)
   extends ReadWriteShard[ConcreteShard] {
 
   def readOperation[A](method: (ConcreteShard => A)) = failover(method(_), loadBalancer())
@@ -37,7 +36,7 @@ class ReplicatingShard[ConcreteShard <: Shard](val shardInfo: ShardInfo, val wei
   private def failover[A](f: ConcreteShard => A, replicas: Seq[ConcreteShard]): A = {
     replicas match {
       case Seq() => throw new ShardOfflineException
-      case Seq(shard, remainder @ _*) => 
+      case Seq(shard, remainder @ _*) =>
         try {
           f(shard)
         } catch {
@@ -48,8 +47,7 @@ class ReplicatingShard[ConcreteShard <: Shard](val shardInfo: ShardInfo, val wei
               case _: ShardRejectedOperationException =>
               case _ =>
                 log.error(shardId, e, "Error on %s: %s", shardId, e)
-                // FIXME kind of an abuse of w3c. make a real event log.
-                eventLogger.map { _.report(Map("db_error-" + shardId -> e.getClass.getName)) }
+                eventLogger("db_error-" + shardId, e.getClass.getName)
             }
             failover(f, remainder)
         }
