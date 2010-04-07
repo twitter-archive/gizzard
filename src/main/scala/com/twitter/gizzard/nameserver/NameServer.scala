3d7f56910d2e94789c7730ee27005ade0f664dc9
Robey Pointer <robey@twitter.com>
2010-04-07 13:53:06 -0700

    fix tests.

--- src/main/scala/com/twitter/gizzard/nameserver/NameServer.scala
+++ src/main/scala/com/twitter/gizzard/nameserver/NameServer.scala
@@ -4,6 +4,7 @@ import java.util.TreeMap
 import scala.collection.mutable
 import shards._
 
+
 class NonExistentShard extends ShardException("Shard does not exist")
 class InvalidShard extends ShardException("Shard has invalid attributes (such as hostname)")
 
