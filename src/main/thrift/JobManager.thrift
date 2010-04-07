061f1934007749757885327338cc5fbb5d40094a
Robey Pointer <robey@twitter.com>
2010-04-07 13:21:21 -0700

    patch to use thirft 0.2.0.

--- src/main/thrift/JobManager.thrift
+++ src/main/thrift/JobManager.thrift
@@ -6,14 +6,14 @@ exception JobException {
 }
 
 service JobManager {
-  void retry_errors() throws(JobException ex)
-  void stop_writes() throws(JobException ex)
-  void resume_writes() throws(JobException ex)
+  void retry_errors() throws(1: JobException ex)
+  void stop_writes() throws(1: JobException ex)
+  void resume_writes() throws(1: JobException ex)
 
-  void retry_errors_for(1: i32 priority) throws(JobException ex)
-  void stop_writes_for(1: i32 priority) throws(JobException ex)
-  void resume_writes_for(1: i32 priority) throws(JobException ex)
+  void retry_errors_for(1: i32 priority) throws(1: JobException ex)
+  void stop_writes_for(1: i32 priority) throws(1: JobException ex)
+  void resume_writes_for(1: i32 priority) throws(1: JobException ex)
 
-  bool is_writing(1: i32 priority) throws(JobException ex)
-  void inject_job(1: i32 priority, 2: string job) throws(JobException ex)
+  bool is_writing(1: i32 priority) throws(1: JobException ex)
+  void inject_job(1: i32 priority, 2: string job) throws(1: JobException ex)
 }
