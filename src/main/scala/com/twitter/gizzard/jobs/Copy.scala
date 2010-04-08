ec57e6278aae1851e3e042fe238e6855a15327fb
Nick Kallen <pivotal@Toemang-2.local>
2010-04-07 17:38:43 -0700

    some cleaner ways of doing count

--- src/main/scala/com/twitter/gizzard/jobs/Copy.scala
+++ src/main/scala/com/twitter/gizzard/jobs/Copy.scala
@@ -14,11 +14,10 @@ object Copy {
 
 trait CopyFactory[S <: shards.Shard] extends ((Int, Int) => Copy[S])
 
-abstract case class Copy[S <: Shard](sourceShardId: Int, destinationShardId: Int, count: Int) extends UnboundJob[(NameServer[S], JobScheduler)] {
+abstract case class Copy[S <: Shard](sourceShardId: Int, destinationShardId: Int, var count: Int) extends UnboundJob[(NameServer[S], JobScheduler)] {
   private val log = Logger.get(getClass.getName)
-  private var nextCount = count
 
-  def toMap = Map("source_shard_id" -> sourceShardId, "destination_shard_id" -> destinationShardId, "count" -> nextCount) ++ serialize
+  def toMap = Map("source_shard_id" -> sourceShardId, "destination_shard_id" -> destinationShardId, "count" -> count) ++ serialize
 
   def finish(nameServer: NameServer[S], scheduler: JobScheduler) {
     nameServer.markShardBusy(destinationShardId, Busy.Normal)
@@ -28,34 +27,31 @@ abstract case class Copy[S <: Shard](sourceShardId: Int, destinationShardId: Int
 
   def apply(environment: (NameServer[S], JobScheduler)) {
     val (nameServer, scheduler) = environment
-    val nextJob = try {
+    try {
       log.info("Copying shard block (type %s) from %d to %d: state=%s",
                getClass.getName.split("\\.").last, sourceShardId, destinationShardId, toMap)
       val sourceShard = nameServer.findShardById(sourceShardId)
       val destinationShard = nameServer.findShardById(destinationShardId)
       // do this on each iteration, so it happens in the queue and can be retried if the db is busy:
       nameServer.markShardBusy(destinationShardId, Busy.Busy)
-      copyPage(sourceShard, destinationShard, count)
+      val nextJob = copyPage(sourceShard, destinationShard, count)
+      nextJob match {
+        case Some(job) => scheduler(job)
+        case None => finish(nameServer, scheduler)
+      }
     } catch {
       case e: NonExistentShard =>
         log.error("Shard block copy failed because one of the shards doesn't exist. Terminating the copy.")
-        return
       case e: ShardTimeoutException if (count > Copy.MIN_COPY) =>
         log.warning("Shard block copy timed out; trying a smaller block size.")
-        nextCount = count / 2
+        count = count / 2
         scheduler(this)
-        return
       case e: ShardDatabaseTimeoutException =>
         log.warning("Shard block copy failed to get a database connection; retrying.")
         scheduler(this)
-        return
       case e: Exception =>
         log.warning("Shard block copy stopped due to exception: %s", e)
         throw e
-    }
-    nextJob match {
-      case Some(job) => scheduler(job)
-      case None => finish(nameServer, scheduler)
     }
   }
 
