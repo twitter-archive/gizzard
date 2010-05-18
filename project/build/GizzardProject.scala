1d6c86f590bffc9a47e6123472e49d9a8aab19b9
Robey Pointer <robey@twitter.com>
2010-05-18 16:43:23 -0700

    use newer querulous.

--- project/build/GizzardProject.scala
+++ project/build/GizzardProject.scala
@@ -14,7 +14,7 @@ class GizzardProject(info: ProjectInfo) extends StandardProject(info) {
   val objenesis = "org.objenesis" % "objenesis" % "1.1"
   val ostrich   = "com.twitter" % "ostrich" % "1.1.15"
   val pool      = "commons-pool" % "commons-pool" % "1.3"
-  val querulous = "com.twitter" % "querulous" % "1.1.8"
+  val querulous = "com.twitter" % "querulous" % "1.1.9"
   val slf4j     = "org.slf4j" % "slf4j-jdk14" % "1.5.2"
   val slf4jApi  = "org.slf4j" % "slf4j-api" % "1.5.2"
   val specs     = "org.scala-tools.testing" % "specs" % "1.6.2.1"
