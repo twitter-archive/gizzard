da502b8b3e462183901fa412fb9fecc05ae1dd39
Robey Pointer <robey@twitter.com>
2010-06-21 12:23:34 -0700

    fix filename.

--- src/test/scala/com/twitter/gizzard/ConfiguredSpecification.scala
+++ src/test/scala/com/twitter/gizzard/ConfiguredSpecification.scala
@@ -3,6 +3,7 @@ package com.twitter.gizzard
 import net.lag.configgy.Configgy
 import org.specs.Specification
 
+
 abstract class ConfiguredSpecification extends Specification {
   Configgy.configure("config/test.conf")
   val config = Configgy.config
