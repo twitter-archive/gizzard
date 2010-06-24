ce3569cad0413229e9deae9265f7c87276136ba6
Kyle Maxwell <kmaxwell@twitter.com>
2010-06-24 13:46:40 -0700

    flesh out the commands

--- src/main/ruby/gizzard/commands.rb
+++ src/main/ruby/gizzard/commands.rb
@@ -1,9 +1,13 @@
 module Gizzard
   class Command
     include Thrift
-    
+
     def self.run(command_name, *args)
-      Gizzard.const_get("#{command_name.capitalize}Command").new(*args).run
+      Gizzard.const_get("#{classify(command_name)}Command").new(*args).run
+    end
+
+    def self.classify(string)
+      string.split(/\W+/).map(&:capitalize).join("")
     end
 
     attr_reader :service, :global_options, :argv, :command_options
@@ -13,12 +17,62 @@ module Gizzard
       @argv            = argv
       @command_options = command_options
     end
-    
+
     def help!(message = nil)
       raise HelpNeededError, message
     end
   end
-  
+
+  class ReloadCommand < Command
+    def run
+      puts "Are you sure? Reloading will affect production services immediately! (Type 'yes')"
+      if gets.chomp == "yes"
+        service.reload_forwardings
+      else
+        STDERR.puts "aborted"
+      end
+    end
+  end
+
+  class LinkCommand < Command
+    def run
+      up_id, down_id, weight = argv
+      help! if argv.length != 3
+      weight = weight.to_i
+      up_id  = ShardId.parse(up_id)
+      down_id  = ShardId.parse(down_id)
+      link = LinkInfo.new(up_id, down_id, weight)
+      service.add_link(link.up_id, link.down_id, link.weight)
+      puts link.to_unix
+    end
+  end
+
+  class UnlinkCommand < Command
+    def run
+      up_id, down_id = argv
+      up_id  = ShardId.parse(up_id)
+      down_id  = ShardId.parse(down_id)
+      service.remove_link(up_id, down_id)
+    end
+  end
+
+  class UnwrapCommand < Command
+    def run
+      help! "No shards specified" if shard_ids.empty?
+      shard_ids.each do |shard_id_string|
+        shard_id   = ShardId.parse(shard_id_string)
+        service.list_upward_links(shard_id).each do |uplink|
+          service.list_downward_links(shard_id).each do |downlink|
+            new_link = service.add_link(uplink.up_id, downlink.down_id, uplink.weight)
+            service.remove_link(uplink.up_id, uplink.down_id)
+            service.remove_link(downlink.up_id, downlink.down_id)
+            puts new_link.to_unix
+          end
+        end
+      end
+    end
+  end
+
   class CreateCommand < Command
     def run
       help! if argv.length != 3
@@ -31,8 +85,7 @@ module Gizzard
       puts shard_id.to_unix
     end
   end
-  
-  
+
   class LinksCommand < Command
     def run
       shard_ids = @argv
@@ -57,13 +110,13 @@ module Gizzard
       end
     end
   end
-  
+
   class WrapCommand < Command
     def self.derive_wrapper_shard_id(shard_info, wrapping_class_name)
       prefix_prefix = wrapping_class_name.split(".").last.downcase.gsub("shard", "") + "_"
       ShardId.new(shard_info.id.hostname, prefix_prefix + shard_info.id.table_prefix)
     end
-    
+
     def run
       class_name, *shard_ids = @argv
       abort "No shards specified" if shard_ids.empty?
@@ -71,23 +124,10 @@ module Gizzard
         shard_id   = ShardId.parse(shard_id_string)
         shard_info = service.get_shard(shard_id)
         service.create_shard(ShardInfo.new(wrapper_id = self.class.derive_wrapper_shard_id(shard_info, class_name), class_name, "", "", 0))
-        
-        # This line of code appears to link wrapping table with itself.  Marcel and Kyle couldn't immediately figure out why.
-        #
-        # $ g wrap com.twitter.service.flock.edges.ReplicatingShard localhost/table_b_14
-        # localhost/table_1277345064
-        # $ g links localhost/table_repl_14
-        # localhost/table_repl_14 localhost/table_a_14  2
-        # localhost/table_repl_14 localhost/table_1277345064  1
-        # $ g links localhost/table_1277345064
-        # localhost/table_1277345064  localhost/table_1277345064  1
-        # localhost/table_repl_14 localhost/table_1277345064  1
-        # localhost/table_1277345064  localhost/table_1277345064  1
-        # $ g links localhost/table_b_14
-        #         
+
+        existing_links = service.list_upward_links(shard_id)
         service.add_link(wrapper_id, shard_id, 1)
-        
-        service.list_upward_links(shard_id).each do |link_info|
+        existing_links.each do |link_info|
           service.add_link(link_info.up_id, wrapper_id, link_info.weight)
           service.remove_link(link_info.up_id, link_info.down_id)
         end
@@ -95,7 +135,7 @@ module Gizzard
       end
     end
   end
-  
+
   class FindCommand < Command
     def run
       service.shards_for_hostname(command_options.shard_host).each do |shard|
--- src/main/ruby/gizzmo.rb
+++ src/main/ruby/gizzmo.rb
@@ -15,32 +15,22 @@ subcommand_options = OpenStruct.new
 argv = nil
 
 subcommands = {
-  'getweight' => OptionParser.new do |opts|
-    opts.banner = "Usage: #{$0} getweight SHARD [MORE SHARD_IDS...]"
-  # ...
-  end,
-  'setweight' => OptionParser.new do |opts|
-    opts.banner = "Usage: #{$0} setweight VALUE SHARD [MORE SHARD_IDS...]"
-  # ...
-  end,
   'create' => OptionParser.new do |opts|
-    opts.banner = "Usage: #{$0} create [options] HOST TABLE_PREFIX CLASS_NAME"    
+    opts.banner = "Usage: #{$0} create [options] HOST TABLE_PREFIX CLASS_NAME"
 
     opts.on("-s", "--source-type=[TYPE]") do |s|
       subcommand_options.source_type = s
     end
-    
+
     opts.on("-d", "--destination-type=[TYPE]") do |s|
       subcommand_options.destination_type = s
     end
   end,
   'wrap' => OptionParser.new do |opts|
     opts.banner = "Usage: #{$0} wrap CLASS_NAME SHARD_ID_TO_WRAP [MORE SHARD_IDS...]"
-  # ...
   end,
   'unwrap' => OptionParser.new do |opts|
-    opts.banner = "Usage: #{$0} unwrap SHARD_ID_TO_REMOVE"
-  # ...
+    opts.banner = "Usage: #{$0} unwrap SHARD_ID_TO_REMOVE [MORE SHARD_IDS]"
   end,
   'find' => OptionParser.new do |opts|
     opts.banner = "Usage: #{$0} find [options]"
@@ -52,10 +42,6 @@ subcommands = {
     opts.on("-H", "--host=HOST", "HOST of shard") do |shard_host|
       subcommand_options.shard_host = shard_host
     end
-  # ...
-  end,
-  'add-link' => OptionParser.new do |opts|
-    opts.banner = "Usage: #{$0} add-link UP_ID DOWN_ID"
   end,
   'links' => OptionParser.new do |opts|
     opts.banner = "Usage: #{$0} links SHARD_ID [MORE SHARD_IDS...]"
@@ -63,14 +49,14 @@ subcommands = {
   'info' => OptionParser.new do |opts|
     opts.banner = "Usage: #{$0} info SHARD_ID [MORE SHARD_IDS...]"
   end,
-  # ...
-  'push' => OptionParser.new do |opts|
-    opts.banner = "Usage: #{$0} push PARENT_SHARD_ID CHILD_SHARD_ID"
-  # ...
+  'reload' => OptionParser.new do |opts|
+    opts.banner = "Usage: #{$0} reload"
   end,
-  'pop' => OptionParser.new do |opts|
-    opts.banner = "Usage: #{$0} pop SHARD_ID_TO_REMOVE [MORE SHARD_IDS...]"
-  # ...
+  'link' => OptionParser.new do |opts|
+    opts.banner = "Usage: #{$0} link PARENT_SHARD_ID CHILD_SHARD_ID WEIGHT"
+  end,
+  'unlink' => OptionParser.new do |opts|
+    opts.banner = "Usage: #{$0} unlink PARENT_SHARD_ID CHILD_SHARD_ID"
   end
 }
 
@@ -151,10 +137,15 @@ else
   rescue HelpNeededError => e
     STDERR.puts e.message if e.class.name != e.message
     STDERR.puts subcommands[subcommand_name]
+    exit 1
+  rescue ThriftClient::Simple::ThriftException => e
+    STDERR.puts e.message
+    exit 1
   end
 
   # include Gizzard::Thrift
   # 20.times do |i|
+  #   i += 20
   #   repl = service.create_shard(ShardInfo.new(repl_id = ShardId.new("localhost", "table_repl_#{i}"), "com.twitter.service.flock.edges.ReplicatingShard", "", "", 0))
   #   a    = service.create_shard(ShardInfo.new(a_id    = ShardId.new("localhost", "table_a_#{i}"), "com.twitter.service.flock.edges.SqlShard", "INT UNSIGNED", "INT UNSIGNED", 0))
   #   b    = service.create_shard(ShardInfo.new(b_id    = ShardId.new("localhost", "table_b_#{i}"), "com.twitter.service.flock.edges.SqlShard", "INT UNSIGNED", "INT UNSIGNED", 0))
