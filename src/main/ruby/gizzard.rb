784487ec282805a4e8ef04b44205a052d84a8912
Kyle Maxwell <kmaxwell@twitter.com>
2010-06-24 17:19:58 -0700

    removed gizzmo code, moved to separate repo, pending amac approval.

--- src/main/ruby/gizzard.rb
+++
@@ -1,6 +0,0 @@
-$: << File.dirname(__FILE__)
-module Gizzard; end
-require "gizzard/consts"
-require "gizzard/thrift"
-require "gizzard/commands"
-require "gizzard/dsl"
--- src/main/ruby/gizzard/consts.rb
+++
@@ -1,6 +0,0 @@
-module Gizzard
-  module Consts
-    WRITE_ONLY_SHARD = "com.twitter.gizzard.shards.WriteOnlyShard"
-    REPLICATING_SHARD = "com.twitter.gizzard.shards.ReplicatingShard"
-  end
-end
