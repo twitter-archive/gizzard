257ec1a8960c8256265aaeb928268325bdbea42a
Kyle Maxwell <kmaxwell@twitter.com>
2010-06-29 11:03:39 -0700

    throw on self-referential links

--- src/main/scala/com/twitter/gizzard/nameserver/SqlShard.scala
+++ src/main/scala/com/twitter/gizzard/nameserver/SqlShard.scala
@@ -122,6 +122,9 @@ class SqlShard(queryEvaluator: QueryEvaluator) extends Shard {
   }
 
   def addLink(upId: ShardId, downId: ShardId, weight: Int) {
+    if(upId == downId) {
+      throw new ShardException("Can't link shard to itself")
+    }
     queryEvaluator.execute("INSERT INTO shard_children (parent_hostname, parent_table_prefix, child_hostname, child_table_prefix, weight) VALUES (?, ?, ?, ?, ?) ON DUPLICATE KEY UPDATE weight=VALUES(weight)",
       upId.hostname, upId.tablePrefix, downId.hostname, downId.tablePrefix, weight)
     // XXX: todo - check loops
