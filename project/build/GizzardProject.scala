0c4adcb0ce2c91838cc0d31c8760a9aeae908a11
Ed Ceaser <eac@twitter.com>
2010-08-18 16:29:38 -0700

    Use the latest ostrich, increment version #

--- project/build/GizzardProject.scala
+++ project/build/GizzardProject.scala
@@ -3,25 +3,25 @@ import com.twitter.sbt.{SubversionRepository, StandardProject}
 
 
 class GizzardProject(info: ProjectInfo) extends StandardProject(info) {
-  val asm       = "asm" % "asm" %  "1.5.3"
-  val cglib     = "cglib" % "cglib" % "2.1_3"
   val configgy  = "net.lag" % "configgy" % "1.6.0"
   val dbcp      = "commons-dbcp" % "commons-dbcp" % "1.2.2"
-  val hamcrest  = "org.hamcrest" % "hamcrest-all" % "1.1"
-  val jmock     = "org.jmock" % "jmock" % "2.4.0"
   val kestrel   = "net.lag" % "kestrel" % "1.2"
   val mysqljdbc = "mysql" % "mysql-connector-java" % "5.1.6"
-  val objenesis = "org.objenesis" % "objenesis" % "1.1"
-  val ostrich   = "com.twitter" % "ostrich" % "1.1.26"
+  val ostrich   = "com.twitter" % "ostrich" % "1.2.1"
   val pool      = "commons-pool" % "commons-pool" % "1.3"
   val querulous = "com.twitter" % "querulous" % "1.1.11"
   val slf4j     = "org.slf4j" % "slf4j-jdk14" % "1.5.2"
   val slf4jApi  = "org.slf4j" % "slf4j-api" % "1.5.2"
-  val specs     = "org.scala-tools.testing" % "specs" % "1.6.2.1"
   val thrift    = "thrift" % "libthrift" % "0.2.0"
   val xrayspecs = "com.twitter" % "xrayspecs" % "1.0.7"
+  val json      = "com.twitter" % "json" % "1.1.3"
 
-  val json = "com.twitter" % "json" % "1.1.3"
+  val specs     = "org.scala-tools.testing" % "specs" % "1.6.2.1" % "test"
+  val objenesis = "org.objenesis" % "objenesis" % "1.1" % "test"
+  val jmock     = "org.jmock" % "jmock" % "2.4.0" % "test"
+  val hamcrest  = "org.hamcrest" % "hamcrest-all" % "1.1" % "test"
+  val asm       = "asm" % "asm" %  "1.5.3" % "test"
+  val cglib     = "cglib" % "cglib" % "2.1_3" % "test"
 
   override def pomExtra =
     <licenses>
