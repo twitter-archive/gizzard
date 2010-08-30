169ee98c8d3eed3926d85bff61d726c600a21c10
Robey Pointer <robey@twitter.com>
2010-08-30 14:12:23 -0700

    add non-package-scoped shard types too. and tests!

---
+++ src/test/scala/com/twitter/gizzard/nameserver/ShardRepositorySpec.scala
@@ -0,0 +1,21 @@
+package com.twitter.gizzard.nameserver
+
+import org.specs.Specification
+import org.specs.mock.{ClassMocker, JMocker}
+import net.lag.configgy.Config
+
+
+object ShardRepositorySpec extends ConfiguredSpecification with JMocker with ClassMocker {
+  "BasicShardRepository" should {
+    val future = mock[Future]
+    val shard = mock[shards.Shard]
+    val constructor = { (shard: shards.ReadWriteShard[shards.Shard]) => shard }
+    val repository = new BasicShardRepository(constructor, future)
+
+    "find a replicating shard" in {
+      repository.factory("ReplicatingShard") must haveClass[shards.ReplicatingShardFactory[shards.Shard]]
+      repository.factory("com.twitter.gizzard.shards.ReplicatingShard") must haveClass[shards.ReplicatingShardFactory[shards.Shard]]
+      repository.factory("com.example.bogis.ReplicatingShard") must throwA[NoSuchElementException]
+    }
+  }
+}
