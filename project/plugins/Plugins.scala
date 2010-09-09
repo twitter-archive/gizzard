6aae422fbba260c367fbc9d9e56ec442f97ba01a
Robey Pointer <robey@twitter.com>
2010-09-08 21:33:54 -0700

    fix plugin imports.

--- project/plugins/Plugins.scala
+++ project/plugins/Plugins.scala
@@ -1,9 +1,7 @@
 import sbt._
 
-
 class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
-  val scalaToolsReleases = "scala-tools.org" at "http://scala-tools.org/repo-releases/"
-  val twitterGithubRepository = "twitter.com" at "http://twitter.github.com/repo/"
-  val twitterNestRepository = "twitter.com" at "http://www.lag.net/nest"
+  val scalaTools = "scala-tools.org" at "http://scala-tools.org/repo-releases/"
+  val lagNet = "twitter.com" at "http://www.lag.net/repo/"
   val defaultProject = "com.twitter" % "standard-project" % "0.7.1"
 }
