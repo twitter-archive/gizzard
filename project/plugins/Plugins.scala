8bd3c67b897042b78fcc24981787b13564eb2ea1
Robey Pointer <robey@twitter.com>
2010-09-08 18:41:44 -0700

    use newer standard-project.

--- project/plugins/Plugins.scala
+++ project/plugins/Plugins.scala
@@ -5,5 +5,5 @@ class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
   val scalaToolsReleases = "scala-tools.org" at "http://scala-tools.org/repo-releases/"
   val twitterGithubRepository = "twitter.com" at "http://twitter.github.com/repo/"
   val twitterNestRepository = "twitter.com" at "http://www.lag.net/nest"
-  val defaultProject = "com.twitter" % "standard-project" % "0.7.0"
+  val defaultProject = "com.twitter" % "standard-project" % "0.7.1"
 }
