235a88fc7b3f463bedb95cdda237f594343d7cd2
eaceaser <eac@twitter.com>
2010-09-19 19:45:44 -0700

    hanging comment

--- project/plugins/Plugins.scala
+++ project/plugins/Plugins.scala
@@ -1,7 +1,6 @@
 import sbt._
 
 class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
-  val scalaTools = "twitter.com" at "http://maven.twttr.com/"
-//  val lagNet = "twitter.com" at "http://www.lag.net/repo/"
+  val twitterMaven = "twitter.com" at "http://maven.twttr.com/"
   val defaultProject = "com.twitter" % "standard-project" % "0.7.5"
 }
