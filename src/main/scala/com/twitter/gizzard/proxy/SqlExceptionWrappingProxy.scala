adfc7e713da1884078d7aad4ff25fc0d16500538
eaceaser <eac@twitter.com>
2010-09-20 15:39:32 -0700

    Add a proxyless way to wrap sqlexceptions w/ shardexceptions

--- src/main/scala/com/twitter/gizzard/proxy/SqlExceptionWrappingProxy.scala
+++ src/main/scala/com/twitter/gizzard/proxy/SqlExceptionWrappingProxy.scala
@@ -5,6 +5,7 @@ import shards.ShardId
 import scala.reflect.Manifest
 import com.twitter.querulous.database.SqlDatabaseTimeoutException
 import com.twitter.querulous.query.SqlQueryTimeoutException
+import com.twitter.querulous.evaluator.{QueryEvaluator, QueryEvaluatorProxy}
 
 
 class SqlExceptionWrappingProxy(shardId: ShardId) extends ExceptionHandlingProxy({e =>
@@ -19,3 +20,20 @@ class SqlExceptionWrappingProxy(shardId: ShardId) extends ExceptionHandlingProxy
       throw e
   }
 })
+
+class ShardExceptionWrappingQueryEvaluator(shardId: ShardId, evaluator: QueryEvaluator) extends QueryEvaluatorProxy(evaluator) {
+  override protected def delegate[A](f: => A) = {
+    try {
+      f
+    } catch {
+      case e: SqlQueryTimeoutException =>
+        throw new shards.ShardTimeoutException(shardId, e)
+      case e: SqlDatabaseTimeoutException =>
+        throw new shards.ShardDatabaseTimeoutException(shardId, e)
+      case e: SQLException =>
+        throw new shards.ShardException(e.toString, e)
+      case e: shards.ShardException =>
+        throw e
+     }
+  }
+}
