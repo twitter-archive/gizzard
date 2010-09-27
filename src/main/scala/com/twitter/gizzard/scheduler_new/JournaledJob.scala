5e34ab7d0f93ef00234885417e391d64f56a92a4
Robey Pointer <robey@twitter.com>
2010-09-27 15:11:23 -0700

    oops!

---
+++ src/main/scala/com/twitter/gizzard/scheduler_new/JournaledJob.scala
@@ -0,0 +1,22 @@
+package com.twitter.gizzard.scheduler_new
+
+import net.lag.logging.Logger
+
+/**
+ * Wrapper for JsonJob that logs jobs after they are successfully executed.
+ */
+class JournaledJob[E](job: JsonJob[E], journaller: String => Unit) extends JsonJob[E] {
+  def toMap = job.toMap
+  val environment = job.environment
+
+  def apply(environment: E) {
+    job(environment)
+    try {
+      journaller(job.toJson)
+    } catch {
+      case e: Exception =>
+        val log = Logger.get(getClass.getName)
+        log.warning(e, "Failed to journal job: %s", job.toJson)
+    }
+  }
+}
