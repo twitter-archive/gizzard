b95b1095314bf7e30dd61a134daf884129386803
Matt Freels <matt@freels.name>
2010-10-06 22:58:58 -0700

    add desc to shard

--- src/main/scala/com/twitter/gizzard/shards/FailingOverShard.scala
+++ src/main/scala/com/twitter/gizzard/shards/FailingOverShard.scala
@@ -3,6 +3,19 @@ package com.twitter.gizzard.shards
 import com.twitter.xrayspecs.Duration
 import com.twitter.gizzard.nameserver.FailingOverLoadBalancer
 
+/*
+ * The FailingOverLoadBalancer splits the shard list into online and offline shards.
+ * Online shards are randomized according to the current weight-based formula. Offline
+ * shards are randomly shuffled. To create the final list, 99% of the time, the first
+ * online shard is first, followed by all offline shards, followed by the rest of the
+ * online shards. 1% of the time, the offline shards are all first, to keep them warm.
+
+ * This shard type helps with replication level 3 with one replica designated as a fallback.
+ * The two online replicas will handle the majority of normal read traffic, but read
+ * retries will fall over to the offline replica rather than the other online one.
+ */
+
+
 class FailingOverShardFactory[ConcreteShard <: Shard](
     readWriteShardAdapter: ReadWriteShard[ConcreteShard] => ConcreteShard,
     future: Option[Future],
