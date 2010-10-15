0a25e361c10c0ce7d93c8fc2d902716b7acf5bed
Robey Pointer <robey@twitter.com>
2010-10-14 17:08:47 -0700

    use the released querulous 1.3.4.

--- project/build/GizzardProject.scala
+++ project/build/GizzardProject.scala
@@ -5,7 +5,7 @@ class GizzardProject(info: ProjectInfo) extends StandardProject(info) with Subve
   val configgy  = "net.lag" % "configgy" % "1.6.4"
   val kestrel   = "net.lag" % "kestrel" % "1.2.2"
   val ostrich   = "com.twitter" % "ostrich" % "1.2.7"
-  val querulous = "com.twitter" %% "querulous" % "1.3.4-SNAPSHOT"
+  val querulous = "com.twitter" %% "querulous" % "1.3.4"
   val slf4j     = "org.slf4j" % "slf4j-jdk14" % "1.5.2"
   val slf4jApi  = "org.slf4j" % "slf4j-api" % "1.5.2"
   val thrift    = "thrift" % "libthrift" % "0.2.0-twitter-1"
