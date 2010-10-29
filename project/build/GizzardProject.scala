10588f8d0366191aa0395571fd3e4158616b2897
Robey Pointer <robey@twitter.com>
2010-10-28 17:49:35 -0700

    use newer kestrel.

--- project/build/GizzardProject.scala
+++ project/build/GizzardProject.scala
@@ -3,7 +3,7 @@ import com.twitter.sbt._
 
 class GizzardProject(info: ProjectInfo) extends StandardProject(info) with SubversionPublisher {
   val configgy  = "net.lag" % "configgy" % "1.6.6"
-  val kestrel   = "net.lag" % "kestrel" % "1.2.2"
+  val kestrel   = "net.lag" % "kestrel" % "1.2.3"
   val ostrich   = "com.twitter" % "ostrich" % "1.2.9"
   val querulous = "com.twitter" %% "querulous" % "1.3.4"
   val slf4j     = "org.slf4j" % "slf4j-jdk14" % "1.5.2"
