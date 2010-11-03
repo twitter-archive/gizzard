efc53d0dcd45c9abd27685e5c75c8c3fe5064303
Robey Pointer <robey@twitter.com>
2010-11-03 15:54:25 -0700

    for debugging purposes, log the contents of jobs that error out. also, log how long it takes to load the nameserver forwardings.

--- src/main/scala/com/twitter/gizzard/nameserver/NameServer.scala
+++ src/main/scala/com/twitter/gizzard/nameserver/NameServer.scala
@@ -68,6 +68,8 @@ class NameServer[S <: shards.Shard](nameServerShard: Shard, shardRepository: Sha
                                     val mappingFunction: Long => Long)
   extends Shard {
 
+  private val log = Logger.get(getClass.getName)
+
   val children = List()
   val shardInfo = new ShardInfo("com.twitter.gizzard.nameserver.NameServer", "", "")
   val weight = 1 // hardcode for now
@@ -89,6 +91,7 @@ class NameServer[S <: shards.Shard](nameServerShard: Shard, shardRepository: Sha
   }
 
   def reload() {
+    log.info("Loading forwarding table...")
     nameServerShard.reload()
 
     val newShardInfos = mutable.Map.empty[ShardId, ShardInfo]
@@ -111,6 +114,7 @@ class NameServer[S <: shards.Shard](nameServerShard: Shard, shardRepository: Sha
     shardInfos = newShardInfos
     familyTree = newFamilyTree
     forwardings = newForwardings
+    log.info("Loading forwarding table is done.")
   }
 
   def findShardById(id: ShardId, weight: Int): S = {
