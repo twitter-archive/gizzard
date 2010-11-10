3888f3b62118bd796ae1a4508ae3ea951ffe90ad
Matt Freels <matt@freels.name>
2010-11-09 20:34:26 -0800

    add TSelectorServer config trait

--- src/main/scala/com/twitter/gizzard/config/TServer.scala
+++ src/main/scala/com/twitter/gizzard/config/TServer.scala
@@ -14,18 +14,38 @@ trait ThreadPool {
   }
 }
 
-trait TNonblockingServer {
+trait TServer {
   def port: Int
   def timeout: Duration
   def idleTimeout: Duration
   def threadPool: ThreadPool
 
-  def apply(processor: thrift.server.TNonblockingServer)
+  def apply(processor: thrift.TProcessor): thrift.server.TServer
 }
 
-trait TSelectorServer extends TNonblockingServer
+trait TSelectorServer extends TServer {
+  def name: String
 
-trait THsHaServer extends TNonblockingServer {
+  def apply(processor: thrift.TProcessor) = {
+    gizzard.thrift.TSelectorServer(name, port, processor, threadPool(), timeout, idleTimeout)
+  }
+}
+
+trait TThreadServer extends TServer {
+  def name: String
+
+  def apply(processor: thrift.TProcessor) = {
+    new gizzard.thrift.ThreadServer(
+      name, port, idleTimeout,
+      threadPool(),
+      new thrift.TProcessorFactory(processor),
+      new thrift.transport.TTransportFactory(),
+      new thrift.protocol.TBinaryProtocol.Factory()
+    )
+  }
+}
+
+trait THsHaServer extends TServer {
   def apply(processor: thrift.TProcessor) = {
     val transport = new thrift.transport.TNonblockingServerSocket(port, timeout.inMillis.toInt)
     val options   = new thrift.server.TNonblockingServer.Options
