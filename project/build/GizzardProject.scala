52294de3f67298d9b4205fd07346589f229edf27
Kyle Maxwell <kmaxwell@twitter.com>
2010-12-02 14:46:09 -0800

    upgrade to thrift 0.5

--- project/build/GizzardProject.scala
+++ project/build/GizzardProject.scala
@@ -11,7 +11,7 @@ class GizzardProject(info: ProjectInfo) extends StandardProject(info) with Subve
 
   val slf4j     = "org.slf4j" % "slf4j-jdk14" % "1.5.2"
   val slf4jApi  = "org.slf4j" % "slf4j-api" % "1.5.2"
-  val thrift    = "thrift" % "libthrift" % "0.2.0-twitter-2"
+  val thrift    = "thrift" % "libthrift" % "0.5.0"
 
   val specs     = "org.scala-tools.testing" % "specs" % "1.6.2.1" % "test"
   val objenesis = "org.objenesis" % "objenesis" % "1.1" % "test"
