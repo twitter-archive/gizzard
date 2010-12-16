9a67564d8b83d6a647e233591aca4589bba9125a
Kyle Maxwell <kmaxwell@twitter.com>
2010-12-15 19:36:36 -0800

    integration test

--- src/test/scala/com/twitter/gizzard/thrift/ShardManagerServiceSpec.scala
+++ src/test/scala/com/twitter/gizzard/thrift/ShardManagerServiceSpec.scala
@@ -60,6 +60,13 @@ object ShardManagerServiceSpec extends ConfiguredSpecification with JMocker with
       manager.create_shard(thriftShardInfo1)
     }
 
+    "dump_nameserver" in {
+      expect {
+        one(nameServer).dumpStructure()
+      }
+      manager.dump_nameserver()
+    }
+
     "get_shard" in {
       expect {
         one(nameServer).getShard(shardInfo1.id) willReturn shardInfo1
