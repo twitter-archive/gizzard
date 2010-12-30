010ba8e4f0be6ad93faee3cf8654864a22a550d5
Matt Freels <matt@freels.name>
2010-12-30 10:42:05 -0800

    allow copy cancellation and add extra flags to shard busy status

--- src/main/scala/com/twitter/gizzard/shards/Busy.scala
+++ src/main/scala/com/twitter/gizzard/shards/Busy.scala
@@ -2,6 +2,8 @@ package com.twitter.gizzard.shards
 
 
 object Busy extends Enumeration {
-  val Normal = Value("Normal")
-  val Busy = Value("Busy")
+  val Normal     = Value("Normal")
+  val Busy       = Value("Busy")
+  val Error      = Value("Error")
+  val Cancelled  = Value("Cancelled")
 }
