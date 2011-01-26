aa5f1437dedc9acf985b826168d25bcfb5334869
Matt Freels <matt@freels.name>
2011-01-25 17:22:51 -0800

    remove FanoutResults

--- src/main/scala/com/twitter/gizzard/test/FakeReadWriteShard.scala
+++ src/main/scala/com/twitter/gizzard/test/FakeReadWriteShard.scala
@@ -1,6 +1,6 @@
 package com.twitter.gizzard.test
 
-import shards.{ReadWriteShard, Shard, ShardInfo, FanoutResults}
+import shards.{ReadWriteShard, Shard, ShardInfo}
 
 class FakeReadWriteShard[S <: Shard](shard: S, val shardInfo: ShardInfo, val weight: Int, val children: Seq[S]) extends ReadWriteShard[S] {
   def readAllOperation[A](method: (S => A)) = Seq(try { Right(method(shard)) } catch { case e => Left(e) })
