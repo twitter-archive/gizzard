a7ca1957b070a56fb49d60d6b606e0d96ffba4b1
Josh Hull <joshbuddy@twitter.com>
2011-04-22 13:45:38 -0700

    cleanup spec

--- src/test/scala/com/twitter/gizzard/integration/RepairSpec.scala
+++ src/test/scala/com/twitter/gizzard/integration/RepairSpec.scala
@@ -40,7 +40,6 @@ class RepairSpec extends IntegrationSpecification with ConfiguredSpecification {
       shard1.put(4, "this")
       shard1.put(5, "is")
       shard1.put(6, "bulk")
-      //server2.testService.put(1, "hi")
       
       val list = new java.util.ArrayList[com.twitter.gizzard.thrift.ShardId]
       list.add(new com.twitter.gizzard.thrift.ShardId(shard1id.hostname, shard1id.tablePrefix))
@@ -59,53 +58,5 @@ class RepairSpec extends IntegrationSpecification with ConfiguredSpecification {
       listElemenets(shard2.getAll(0, 100)._1) must eventually(
         verify(s => s sameElements listElemenets(shard3.getAll(0, 100)._1)))
     }
-
-    //val replicatingShardId = ShardId("localhost", "replicating_forward_1")
-    //val (shard1id, shard2id, shard3id) = (ShardId("localhost", "forward_1_1"), ShardId("localhost", "forward_2_1"), ShardId("localhost", "forward_3_1"))
-    //lazy val shard1 = nameServer.findShardById(shard1id)
-    //lazy val shard2 = nameServer.findShardById(shard2id)
-    //lazy val shard3 = nameServer.findShardById(shard3id)
-    //
-    //"differing shards should become the same" in {
-    //  shard1.add(1L, 2L, 1L, Time.now) // same
-    //  shard2.add(1L, 2L, 1L, Time.now)
-    //
-    //  shard1.archive(2L, 1L, 2L, Time.now) // one archived, one normal
-    //  shard2.add(2L, 1L, 2L, Time.now)
-    //  shard3.add(2L, 1L, 2L, Time.now)
-    //
-    //  shard1.add(1L, 3L, 3L, Time.now) // only on one shard
-    //  shard3.archive(1L, 3L, 3L, Time.now)
-    //
-    //  shard2.add(1L, 4L, 4L, Time.now)  // only on two shard
-    //
-    //  shard3.negate(3L, 1L, 5L, Time.now)  // only on two shard
-    //
-    //  // bulk
-    //  shard1.add(5L, 2L, 1L, Time.now) // same
-    //  shard1.add(6L, 2L, 1L, Time.now) // same
-    //  shard1.add(7L, 2L, 1L, Time.now) // same
-    //  shard1.add(8L, 2L, 1L, Time.now) // same
-    //  shard1.add(9L, 2L, 1L, Time.now) // same
-    //  shard1.add(10L, 2L, 1L, Time.now) // same
-    //
-    //
-    //
-    //  val list = new java.util.ArrayList[com.twitter.gizzard.thrift.ShardId]
-    //  list.add(new com.twitter.gizzard.thrift.ShardId(shard1id.hostname, shard1id.tablePrefix))
-    //  list.add(new com.twitter.gizzard.thrift.ShardId(shard2id.hostname, shard2id.tablePrefix))
-    //  list.add(new com.twitter.gizzard.thrift.ShardId(shard3id.hostname, shard3id.tablePrefix))
-    //  manager.repair_shard(list)
-    //  def listElemenets(list: Seq[Edge]) = {
-    //    list.map((e) => (e.sourceId, e.destinationId, e.state))
-    //  }
-    //
-    //  listElemenets(shard1.selectAll(EdgeCursor.Start, Repair.COUNT)._1) must eventually(
-    //    verify(s => s sameElements listElemenets(shard2.selectAll(EdgeCursor.Start, Repair.COUNT)._1)))
-    //  listElemenets(shard1.selectAll(EdgeCursor.Start, Repair.COUNT)._1) must eventually(
-    //    verify(s => s sameElements listElemenets(shard3.selectAll(EdgeCursor.Start, Repair.COUNT)._1)))
-    //  listElemenets(shard2.selectAll(EdgeCursor.Start, Repair.COUNT)._1) must eventually(
-    //    verify(s => s sameElements listElemenets(shard3.selectAll(EdgeCursor.Start, Repair.COUNT)._1)))
-    //}
   }
 }
