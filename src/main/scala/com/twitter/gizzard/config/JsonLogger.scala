b869c146540d63169340e4753fb8f6f7a2425335
eaceaser <eac@twitter.com>
2011-05-31 14:16:45 -0700

    get rid of the json stuff. its bad

--- src/main/scala/com/twitter/gizzard/config/JsonLogger.scala
+++
@@ -1,8 +0,0 @@
-package com.twitter.gizzard
-package config
-
-import com.twitter.logging.config.FormatterConfig
-
-object JsonFormatterConfig extends FormatterConfig {
-  override def apply() = JsonFormatter
-}
