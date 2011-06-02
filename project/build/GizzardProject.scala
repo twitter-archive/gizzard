cd7f6905ec57fa79ca35de0e1d8c54b32f705c93
eaceaser <eac@twitter.com>
2011-06-02 14:38:39 -0700

    use stats topical release for querulous, change version

--- project/build/GizzardProject.scala
+++ project/build/GizzardProject.scala
@@ -9,7 +9,7 @@ with SubversionPublisher {
   override def filterScalaJars = false
   val scalaTools = "org.scala-lang" % "scala-compiler" % "2.8.1"
 
-  val querulous  = "com.twitter" % "querulous" % "2.1.5"
+  val querulous  = "com.twitter" % "querulous" % "2.1.6-stats-alpha0"
 
   //val kestrel     = "net.lag" % "kestrel" % "1.2.7"
   // remove when moved to libkestrel
