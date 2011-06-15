0596101c6a33c4866318b20d8dbbd65e78a7d0f3
Matt Freels <matt@freels.name>
2011-06-14 18:08:35 -0700

    move out shard manager as well

--- src/main/scala/com/twitter/gizzard/nameserver/NameServer.scala
+++ src/main/scala/com/twitter/gizzard/nameserver/NameServer.scala
@@ -2,10 +2,6 @@ package com.twitter.gizzard.nameserver
 
 import java.util.TreeMap
 import scala.collection.mutable
-import com.twitter.util.Time
-import com.twitter.conversions.time._
-import com.twitter.querulous.StatsCollector
-import com.twitter.querulous.evaluator.QueryEvaluatorFactory
 import com.twitter.logging.Logger
 import com.twitter.gizzard.shards._
 
@@ -41,42 +37,6 @@ object TreeUtils {
 class NameServerSource(shard: RoutingNode[com.twitter.gizzard.nameserver.Shard]) {
   def reload()       { shard.write.foreach(_.reload()) }
   def currentState() = shard.read.any(_.currentState())
-}
-
-class ShardManager(shard: RoutingNode[com.twitter.gizzard.nameserver.Shard]) {
-  def dumpStructure(tableIds: Seq[Int]) = shard.read.any(_.dumpStructure(tableIds))
-
-  def createShard(shardInfo: ShardInfo)            { shard.write.foreach(_.createShard(shardInfo)) }
-  def deleteShard(id: ShardId)                     { shard.write.foreach(_.deleteShard(id)) }
-  def markShardBusy(id: ShardId, busy: Busy.Value) { shard.write.foreach(_.markShardBusy(id, busy)) }
-
-  // XXX: removing this causes CopyJobSpec to fail.
-  @throws(classOf[NonExistentShard])
-  def getShard(id: ShardId)               = shard.read.any(_.getShard(id))
-  def shardsForHostname(hostname: String) = shard.read.any(_.shardsForHostname(hostname))
-  def listShards()                        = shard.read.any(_.listShards())
-  def getBusyShards()                     = shard.read.any(_.getBusyShards())
-
-
-  def addLink(upId: ShardId, downId: ShardId, weight: Int) { shard.write.foreach(_.addLink(upId, downId, weight)) }
-  def removeLink(upId: ShardId, downId: ShardId)           { shard.write.foreach(_.removeLink(upId, downId)) }
-
-  def listUpwardLinks(id: ShardId)   = shard.read.any(_.listUpwardLinks(id))
-  def listDownwardLinks(id: ShardId) = shard.read.any(_.listDownwardLinks(id))
-  def listLinks()                    = shard.read.any(_.listLinks())
-
-
-  def setForwarding(forwarding: Forwarding)             { shard.write.foreach(_.setForwarding(forwarding)) }
-  def removeForwarding(f: Forwarding)                   { shard.write.foreach(_.removeForwarding(f)) }
-  def replaceForwarding(oldId: ShardId, newId: ShardId) { shard.write.foreach(_.replaceForwarding(oldId, newId)) }
-
-  def getForwarding(tableId: Int, baseId: Long) = shard.read.any(_.getForwarding(tableId, baseId))
-  def getForwardingForShard(id: ShardId)        = shard.read.any(_.getForwardingForShard(id))
-  def getForwardings()                          = shard.read.any(_.getForwardings())
-
-
-  def listHostnames() = shard.read.any(_.listHostnames())
-  def listTables()    = shard.read.any(_.listTables())
 }
 
 class NameServer(val shard: RoutingNode[com.twitter.gizzard.nameserver.Shard], val mappingFunction: Long => Long) {
