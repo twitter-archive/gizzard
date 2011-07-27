43c79dacb1aab098cfd0d8805a971da07dfeddf3
Matt Freels <matt@freels.name>
2011-07-26 18:29:39 -0700

    move replicating shard to wrapper nodes file

--- src/main/scala/com/twitter/gizzard/shards/WrapperRoutingNodes.scala
+++ src/main/scala/com/twitter/gizzard/shards/WrapperRoutingNodes.scala
@@ -1,46 +1,76 @@
 package com.twitter.gizzard.shards
 
+import com.twitter.gizzard.nameserver.LoadBalancer
+
+
+// ReplicatingShard. Forward and fail over to other children
+
+case class ReplicatingShard[T](shardInfo: ShardInfo, weight: Int, children: Seq[RoutingNode[T]])
+extends RoutingNode[T] {
+  protected def loadBalancer() = new LoadBalancer(children).apply()
+
+  protected[shards] def collectedShards(readOnly: Boolean) = {
+    loadBalancer() flatMap { _.collectedShards(readOnly) }
+  }
+}
+
 
 // Base class for all read/write flow wrapper shards
 
 abstract class WrapperRoutingNode[T] extends RoutingNode[T] {
-
   protected def leafTransform(l: RoutingNode.Leaf[T]): RoutingNode.Leaf[T]
 
-  protected[shards] def collectedShards(readOnly: Boolean) = children flatMap { _.collectedShards(readOnly).map(leafTransform) }
+  protected[shards] def collectedShards(readOnly: Boolean) = {
+    children flatMap { _.collectedShards(readOnly).map(leafTransform) }
+  }
 }
 
 
 // BlockedShard. Refuse and fail all traffic.
 
-case class BlockedShard[T](shardInfo: ShardInfo, weight: Int, children: Seq[RoutingNode[T]]) extends WrapperRoutingNode[T] {
-  protected def leafTransform(l: RoutingNode.Leaf[T]) = l.copy(readBehavior = RoutingNode.Deny, writeBehavior = RoutingNode.Deny)
+case class BlockedShard[T](shardInfo: ShardInfo, weight: Int, children: Seq[RoutingNode[T]])
+extends WrapperRoutingNode[T] {
+  protected def leafTransform(l: RoutingNode.Leaf[T]) = {
+    l.copy(readBehavior = RoutingNode.Deny, writeBehavior = RoutingNode.Deny)
+  }
 }
 
 
 // BlackHoleShard. Silently refuse all traffic.
 
-case class BlackHoleShard[T](shardInfo: ShardInfo, weight: Int, children: Seq[RoutingNode[T]]) extends WrapperRoutingNode[T] {
-  protected def leafTransform(l: RoutingNode.Leaf[T]) = l.copy(readBehavior = RoutingNode.Ignore, writeBehavior = RoutingNode.Ignore)
+case class BlackHoleShard[T](shardInfo: ShardInfo, weight: Int, children: Seq[RoutingNode[T]])
+extends WrapperRoutingNode[T] {
+  protected def leafTransform(l: RoutingNode.Leaf[T]) = {
+    l.copy(readBehavior = RoutingNode.Ignore, writeBehavior = RoutingNode.Ignore)
+  }
 }
 
 
 // WriteOnlyShard. Fail all read traffic.
 
-case class WriteOnlyShard[T](shardInfo: ShardInfo, weight: Int, children: Seq[RoutingNode[T]]) extends WrapperRoutingNode[T] {
-  protected def leafTransform(l: RoutingNode.Leaf[T]) = l.copy(readBehavior = RoutingNode.Deny)
+case class WriteOnlyShard[T](shardInfo: ShardInfo, weight: Int, children: Seq[RoutingNode[T]])
+extends WrapperRoutingNode[T] {
+  protected def leafTransform(l: RoutingNode.Leaf[T]) = {
+    l.copy(readBehavior = RoutingNode.Deny)
+  }
 }
 
 
 // ReadOnlyShard. Fail all write traffic.
 
-case class ReadOnlyShard[T](shardInfo: ShardInfo, weight: Int, children: Seq[RoutingNode[T]]) extends WrapperRoutingNode[T] {
-  protected def leafTransform(l: RoutingNode.Leaf[T]) = l.copy(writeBehavior = RoutingNode.Deny)
+case class ReadOnlyShard[T](shardInfo: ShardInfo, weight: Int, children: Seq[RoutingNode[T]])
+extends WrapperRoutingNode[T] {
+  protected def leafTransform(l: RoutingNode.Leaf[T]) = {
+    l.copy(writeBehavior = RoutingNode.Deny)
+  }
 }
 
 
 // SlaveShard. Silently refuse all write traffic.
 
-case class SlaveShard[T](shardInfo: ShardInfo, weight: Int, children: Seq[RoutingNode[T]]) extends WrapperRoutingNode[T] {
-  protected def leafTransform(l: RoutingNode.Leaf[T]) = l.copy(writeBehavior = RoutingNode.Ignore)
+case class SlaveShard[T](shardInfo: ShardInfo, weight: Int, children: Seq[RoutingNode[T]])
+extends WrapperRoutingNode[T] {
+  protected def leafTransform(l: RoutingNode.Leaf[T]) = {
+    l.copy(writeBehavior = RoutingNode.Ignore)
+  }
 }
