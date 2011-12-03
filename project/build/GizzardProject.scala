21870b0421874c6eaa40e005388a8ea96cacfa9a
Matt Freels <matt@freels.name>
2011-12-02 17:39:42 -0800

    updated finagle and querulous

--- project/build/GizzardProject.scala
+++ project/build/GizzardProject.scala
@@ -6,19 +6,22 @@ with CompileThriftFinagle
 with DefaultRepos
 with SubversionPublisher {
 
+  def finagleVersion = "1.9.10"
+
   override def filterScalaJars = false
   val scalaTools = "org.scala-lang" % "scala-compiler" % "2.8.1"
 
-  val querulous  = "com.twitter" % "querulous" % "2.6.2"
+  val querulous  = "com.twitter" % "querulous" % "2.6.5"
 
   //val kestrel     = "net.lag" % "kestrel" % "1.2.7"
   // remove when moved to libkestrel
   val twitterActors = "com.twitter" % "twitteractors_2.8.0" % "2.0.1"
 
-  val finagleThrift   = "com.twitter"          % "finagle-thrift"     % "1.9.7"
-  val finagleOstrich4 = "com.twitter"          % "finagle-ostrich4"   % "1.9.7"
-  val jackson         = "org.codehaus.jackson" % "jackson-core-asl"   % "1.6.7"
-  val jacksonMap      = "org.codehaus.jackson" % "jackson-mapper-asl" % "1.6.7"
+  val finagleThrift   = "com.twitter"          % "finagle-thrift"     % "1.9.10"
+  val finagleOstrich4 = "com.twitter"          % "finagle-ostrich4"   % "1.9.10"
+
+  val jackson         = "org.codehaus.jackson" % "jackson-core-asl"   % "1.9.2"
+  val jacksonMap      = "org.codehaus.jackson" % "jackson-mapper-asl" % "1.9.2"
 
   val slf4j      = "org.slf4j" % "slf4j-jdk14" % "1.5.2" //intransitive
   val slf4jApi   = "org.slf4j" % "slf4j-api"   % "1.5.2" //intransitive
