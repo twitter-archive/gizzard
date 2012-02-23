ed807b165c91af1f8353ef2c307c921ede18fee3
Melvin Wang <wangmeng@twitter.com>
2012-02-23 11:26:08 -0800

    rename the metric to job-bad-count

--- src/main/scala/com/twitter/gizzard/scheduler/JobScheduler.scala
+++ src/main/scala/com/twitter/gizzard/scheduler/JobScheduler.scala
@@ -167,7 +167,7 @@ extends Process with JobConsumer {
             job.errorMessage = e.toString
             if (job.errorCount > errorLimit) {
               badJobQueue.put(job)
-              Stats.incr("bad-job-count")
+              Stats.incr("job-bad-count")
             } else {
               errorQueue.put(job)
             }
