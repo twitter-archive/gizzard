eb15b115ec3c45f26ec974b1796644d1c80446cf
Raghavendra Prabhu <rvp@twitter.com>
2012-03-30 01:58:48 -0700

    Upgrade to querulous 2.7.6

--- project/build/GizzardProject.scala
+++ project/build/GizzardProject.scala
@@ -11,7 +11,7 @@ with SubversionPublisher {
   override def filterScalaJars = false
   val scalaTools = "org.scala-lang" % "scala-compiler" % "2.8.1"
 
-  val querulous  = "com.twitter" % "querulous" % "2.7.5"
+  val querulous  = "com.twitter" % "querulous" % "2.7.6"
 
   //val kestrel     = "net.lag" % "kestrel" % "1.2.7"
   // remove when moved to libkestrel
